18777b728fe24cbf7ea3db4ebe07268e0e710c80
Morgan Marshall <28496781+MorganMarshall@users.noreply.github.com>
2018-07-15 15:52:39 -0400

    fix port

--- main.go
+++ main.go
@@ -7,5 +7,5 @@ import (
 
 func main() {
 	// Simple static webserver:
-	log.Fatal(http.ListenAndServe(":" + os.Getenv("PORT"), router))
+	log.Fatal(http.ListenAndServe(":", http.FileServer(http.Dir("./"))))
 }
