d46636599a3df971feb5d5ee9e2f833f2d2b0e73
Morgan Marshall <28496781+MorganMarshall@users.noreply.github.com>
2018-07-15 19:59:29 -0400

    add plus port

--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	// Simple static webserver:
-	log.Fatal(http.ListenAndServe(":$PORT", http.FileServer(http.Dir("./"))))
+
+	port := os.Getenv("PORT")
+
+	log.Fatal(http.ListenAndServe(":" + port, http.FileServer(http.Dir("./"))))
 }
 
 
