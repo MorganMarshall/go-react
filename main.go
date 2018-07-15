021f49fdce38a4c29cfec23949f5a83b382a3d9b
Morgan Marshall <28496781+MorganMarshall@users.noreply.github.com>
2018-07-15 16:58:19 -0400

    add os

--- main.go
+++ main.go
@@ -3,9 +3,21 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	
 )
 
 func main() {
 	// Simple static webserver:
-	log.Fatal(http.ListenAndServe(":$PORT", http.FileServer(http.Dir("./src"))))
+	log.Fatal(http.ListenAndServe(GetPort(), http.FileServer(http.Dir("./src"))))
 }
+
+
+func GetPort() string {
+	 	var port = os.Getenv("PORT")
+		// Set a default port if there is nothing in the environment
+	 	if port == "" {
+ 		port = "4747"
+	 	}
+	 	return ":" + port
+	 }
