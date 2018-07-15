aa58e0a0fe0b4ae842f09fb5b28d9b17e3dddd8d
Morgan Marshall <28496781+MorganMarshall@users.noreply.github.com>
2018-07-15 16:14:43 -0400

    remove :

--- main.go
+++ main.go
@@ -7,5 +7,5 @@ import (
 
 func main() {
 	// Simple static webserver:
-	log.Fatal(http.ListenAndServe(":$PORT", http.FileServer(http.Dir("./"))))
+	log.Fatal(http.ListenAndServe("$PORT", http.FileServer(http.Dir("./"))))
 }
