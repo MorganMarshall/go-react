8ac37c46975e30c57b8f2e01d4454bee47b62edb
Morgan Marshall <28496781+MorganMarshall@users.noreply.github.com>
2018-07-15 18:35:39 -0400

    add /.

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	// Simple static webserver:
 	port := ":" + os.Getenv("PORT")
-	log.Fatal(http.ListenAndServe(port, http.FileServer(http.Dir("/"))))
+	log.Fatal(http.ListenAndServe(port, http.FileServer(http.Dir("./"))))
 }
 
 
