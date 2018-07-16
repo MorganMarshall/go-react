7828fcb8b369734fd8c4b90b5a2cc9c0afb39943
Morgan Marshall <28496781+MorganMarshall@users.noreply.github.com>
2018-07-15 23:46:47 -0400

    add "PORT"

--- main.go
+++ main.go
@@ -4,26 +4,11 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"fmt"
 )
-
-func determineListenAddress() (string, error) {
-	port := os.Getenv("PORT")
-	if port == "" {
-	  return "", fmt.Errorf("$PORT not set")
-	}
-	return ":" + port, nil
-  }
 
 func main() {
 	// Simple static webserver:
-	addr, err := determineListenAddress()
-  if err != nil {
-    log.Fatal(err)
-  }
-
-
-	log.Fatal(http.ListenAndServe(addr, http.FileServer(http.Dir("./"))))
+	log.Fatal(http.ListenAndServe(":" + os.Getenv("PORT"), http.FileServer(http.Dir("./"))))
 }
 
 
