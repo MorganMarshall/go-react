de3bf26bdd505f584c937036b7eae982068a3f17
Morgan Marshall <28496781+MorganMarshall@users.noreply.github.com>
2018-07-16 21:50:00 -0400

    fixed

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	// Simple static webserver:
-	port := os.Getenv("PORT")
+	port := os.Getenv("$PORT")
 
 	log.Fatal(http.ListenAndServe(":" + port, http.FileServer(http.Dir("./"))))
 }
